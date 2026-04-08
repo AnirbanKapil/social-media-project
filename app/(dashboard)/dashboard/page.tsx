e3dc5eaa4fff7f66c6d33b26bd0e9ef9ae829a06
Anirban Kapil <anirbankapil@gmail.com>
2026-04-08 21:08:03 +0530

    Add react-icons dependency and create Dashboard component with sidebar menu

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,67 @@
+
+import React from "react";
+import { IoHomeOutline } from "react-icons/io5";
+import { GoHash } from "react-icons/go";
+import { FaRegBell , FaRegEnvelope , FaRegBookmark , FaRegUser } from "react-icons/fa";
+import { LuCircleDollarSign } from "react-icons/lu";
+import { SlOptions } from "react-icons/sl";
+
+
+interface SideBarButtons {
+    title : string,
+    icon  : React.ReactNode
+}
+
+
+const sideBarMenuItems : SideBarButtons[] = [
+    {title : "Home",
+     icon  : <IoHomeOutline />   
+    },
+    { title: "Explore",
+        icon : <GoHash />
+    },
+    {
+        title : "Notifications",
+        icon : <FaRegBell />
+       },
+       {
+        title : "Messages",
+        icon : <FaRegEnvelope />
+       },
+       {
+        title : "Bookmarks",
+        icon : <FaRegBookmark />
+       },
+       {
+        title : "Profile",
+        icon :  <FaRegUser />
+       },
+       {
+        title : "Verify",
+        icon : <LuCircleDollarSign />
+       },
+       {
+        title : "Options",
+        icon : <SlOptions />
+       }
+]
+
+
+
+
+export default function Dashboard () {
+    return (
+        <div className="grid grid-cols-12 w-screen h-screen px-52">
+            <div className="col-span-3">
+               <ul> 
+               {sideBarMenuItems.map((itm)=> <li className="flex justify-center" key={itm.title}>
+                                                <div className="m-2">{itm.title}</div>
+                                                <div className="m-3">{itm.icon}</div> 
+                                             </li>)}
+               </ul>
+            </div>
+            <div className="col-span-6 border-r border-l">Feeds</div>
+            <div className="col-span-3">Happening Now</div>
+        </div>
+    )
+}
