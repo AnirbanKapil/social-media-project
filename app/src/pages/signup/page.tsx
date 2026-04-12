4f80c0ab6fd38110bc479d75baaf246da5c696fd
Anirban Kapil <anirbankapil@gmail.com>
2026-04-12 21:25:19 +0530

    feat(signup): implement user input handling and submission for registration

--- app/src/pages/signup/page.tsx
+++ app/src/pages/signup/page.tsx
@@ -1,21 +1,47 @@
 "use client"
 
+import { useState } from "react"
 import { Card } from "../../components/card"
-
-
+import axios from "axios";
+import { useRouter } from "next/navigation"
 
 
 
 
 export default  function SignUp () {
-      return (
+  
+  const router = useRouter();
+  const [user , setUser] = useState({
+    username : "",
+    password : "",
+    email : ""
+  });
+
+  const handleSubmit =async () =>{
+    try {
+      await axios.post("/api/auth/register",user);
+      setTimeout(()=>{
+                router.push("/api/auth/signin") 
+             },2000)
+    } catch (error : any) {
+      console.log(error)
+      return null;
+    }
+  }
+  
+  return (
         <div className="">
             <Card title="SignUp">
               <div className="flex flex-col">
-                 <input className="m-2 p-3 border-b-2 " type="text" placeholder="username"/> 
-                 <input className="m-2 p-3 border-b-2" type="text" placeholder="email"/> 
-                 <input className="m-2 p-3 border-b-2" type="password" placeholder="password"/> 
-                 <button className="cursor-pointer">SignUp</button>
+                 <input className="m-2 p-3 border-b-2 " type="text" placeholder="username" value={user.username} 
+                 onChange={(e)=> setUser({...user , username : e.target.value})}/> 
+                 <input className="m-2 p-3 border-b-2" type="text" placeholder="email" value={user.email} 
+                 onChange={(e)=> setUser({...user , email : e.target.value})}/> 
+                 <input className="m-2 p-3 border-b-2" type="password" placeholder="password" value={user.password} 
+                 onChange={(e) => setUser({...user , password : e.target.value})}/> 
+                 <button className="border border-gray-300 rounded-lg mb-4 p-2 m-3" onClick={handleSubmit}>
+                  SignUp
+                  </button>
               </div>
             </Card>
         </div>
