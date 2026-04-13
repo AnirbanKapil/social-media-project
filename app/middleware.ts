4baef8db248605cda9afe61eff1489078c2fd55b
Anirban Kapil <anirbankapil@gmail.com>
2026-04-13 20:49:37 +0530

    feat(middleware): refine authorization logic and public path handling

--- app/middleware.ts
+++ app/middleware.ts
@@ -3,21 +3,21 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 export function middleware (request : NextRequest) {
-   const path = request.nextUrl.pathname
+   const path = request.nextUrl.pathname;
 
-   const publicPath = ""
-   const token = request.cookies.get("token")?.value || ""
-   if(publicPath && token){
-    return NextResponse.redirect(new URL("/",request.nextUrl))
+   const publicPath = ["/api/auth" , "/signup"];
+   const isPublic = publicPath.some((p)=> path.startsWith(p))
+   const token = request.cookies.get("token")?.value
+   if(isPublic && token){
+    return NextResponse.redirect(new URL("/dashboard",request.nextUrl))
    }
-   if(!publicPath && !token){
-    return NextResponse.redirect(new URL("/login",request.nextUrl))
+   if(!isPublic && !token){
+    return NextResponse.redirect(new URL("/api/auth/signin",request.nextUrl))
    }
+   return NextResponse.next();
 }
 
 
 export const config = {
-    matcher : [
-        "/",
-    ]
+    matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"]
 }
