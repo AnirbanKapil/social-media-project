f053f19c2c3c82081d51010765dce74283939489
Anirban Kapil <anirbankapil@gmail.com>
2026-05-07 23:23:23 +0530

    feat(post): implement createPost mutation and associated type definitions

---
+++ graphql/modules/post/resolvers.ts
@@ -0,0 +1,30 @@
+import prisma from "@/lib/prisma";
+
+interface CreatePostPayload {
+   content: string;
+   imgURL?: string;
+}
+
+
+
+const mutations = {
+    createPost:async (parent:any, {payload}:{payload : CreatePostPayload}, ctx:any) => {
+
+        if(!ctx.user){
+            throw new Error("Not authenticated!!")
+        };
+        
+        const post = await prisma.post.create({
+            data: {
+                content : payload.content,
+                imgURL  : payload.imgURL,
+                author  : {connect :{ id: ctx.user.id }}
+            }
+        })
+
+        return post;
+
+    }
+};
+
+export const resolvers = { mutations }
--- graphql/modules/post/typeDefs.ts
+++ graphql/modules/post/typeDefs.ts
@@ -1,6 +1,11 @@
 import { gql } from "graphql-tag";
 
 export const postTypeDefs = gql`
+  
+  input CreatePostData {
+    content: String!
+    imgURL: String
+  }
 
   type Post {
     id: Int!
