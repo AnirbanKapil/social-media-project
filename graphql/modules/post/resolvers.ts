445e9b56024a7192b0417a6144c334dff7407656
Anirban Kapil <anirbankapil@gmail.com>
2026-05-14 11:55:56 +0530

    feat(post): update createPost mutation to use session for user authentication and payload structure

--- graphql/modules/post/resolvers.ts
+++ graphql/modules/post/resolvers.ts
@@ -8,16 +8,16 @@ import { postQueries } from "./queries";
 export const postResolvers= {
     Mutation: {
     createPost:async (parent : any, payload : any, ctx:any) => {
-
-        if(!ctx.user){
+        console.log("Creating post with payload:", payload.payload.content);
+        if(!ctx.session?.user){
             throw new Error("Not authenticated!!")
         };
         
         const post = await prisma.post.create({
             data: {
-                content : payload.content,
-                imgURL  : payload.imgURL,
-                author  : {connect :{ id: ctx.user.id }}
+                content : payload.payload.content,
+                imgURL  : payload.payload.imgURL || null,
+                author  : {connect :{ id: ctx.session.user.id }}
             }
         });
         
