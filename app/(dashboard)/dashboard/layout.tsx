c24aeea55263fdeca7e37b3c2a55922e48cb21b4
Anirban Kapil <anirbankapil@gmail.com>
2026-05-20 23:38:45 +0530

    feat(layout): create DashboardLayout component for consistent layout structure

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+
+import { NavBar } from "@/app/src/components/navbar";
+import { WhatsHappening } from "@/app/src/components/whatsHappening";
+
+
+import { SideBarMenuItems } from "@/app/src/components/sideBarMenu";
+
+
+
+
+
+export default function DashboardLayout({children}: {children: React.ReactNode}) {
+    
+     
+     
+    
+
+     return (
+     <div>
+        <NavBar />
+        <div className="grid grid-cols-12 w-screen h-screen px-52 overflow-hidden">
+            <div className="col-span-3 mt-10 sticky top-0 h-screen">
+               <ul> 
+               {SideBarMenuItems.map((itm)=> <li className="flex justify-center cursor-pointer 
+                                                hover:bg-blue-400 hover:scale-120 transition-transform duration-300 w-fit rounded-lg" 
+                                                            key={itm.title}>
+                                                   <div className="m-6 text-2xl">{itm.title}</div>
+                                                   <div className="m-7 text-2xl">{itm.icon}</div>    
+                                             </li>)}
+               </ul>
+            </div>
+            <div className="col-span-6 border-r border-l border-gray-600 overflow-y-auto no-scrollbar">
+              {children}
+            </div>
+            <div className="col-span-3 mt-10 sticky top-0 h-screen">
+                <WhatsHappening />
+            </div>
+        </div>
+     </div>    
+    )
+}
--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,48 +1,26 @@
-"use client";
+"use client"
 
-import React from "react";
-import { useState } from "react";
-import { Feeds } from "@/app/src/components/feeds";
-import { NavBar } from "@/app/src/components/navbar";
-import { WhatsHappening } from "@/app/src/components/whatsHappening";
 import { Publish } from "@/app/src/components/publish";
+import { Feeds } from "@/app/src/components/feeds";
 import { useGetAllPostsQuery } from "@/lib/generated";
-import { SideBarMenuItems } from "@/app/src/components/sideBarMenu";
 
+export default function FeedsPage () {
 
+    const { data, isLoading, error } = useGetAllPostsQuery({});
+    if (isLoading) return <p>Loading...</p>;
 
-
-
-export default function Dashboard () {
-     const { data, isLoading, error } = useGetAllPostsQuery({});
-     
-     if (isLoading) return <p>Loading...</p>;
     
-     if (error) {
+    if (error) {
       const err = error as Error;
-      return <p>Error: {err.message}</p>;
-     } 
-     
-     const posts = data?.getAllPosts;
+        return <p>Error: {err.message}</p>;
+    } 
+         
+    const posts = data?.getAllPosts;
+    
+    if (!posts) return <p>No data</p>;
 
-      if (!posts) return <p>No data</p>;
-
-     return (
-     <div>
-        <NavBar />
-        <div className="grid grid-cols-12 w-screen h-screen px-52 overflow-hidden">
-            <div className="col-span-3 mt-10 sticky top-0 h-screen">
-               <ul> 
-               {SideBarMenuItems.map((itm)=> <li className="flex justify-center cursor-pointer 
-                                                hover:bg-blue-400 hover:scale-120 transition-transform duration-300 w-fit rounded-lg" 
-                                                            key={itm.title}>
-                                                   <div className="m-6 text-2xl">{itm.title}</div>
-                                                   <div className="m-7 text-2xl">{itm.icon}</div>    
-                                             </li>)}
-               </ul>
-            </div>
-            <div className="col-span-6 border-r border-l border-gray-600 overflow-y-auto no-scrollbar">
-                <div className="mt-16 mb-10">
+    return (
+          <div className="mt-16 mb-10">
                     <Publish />
                      {posts.map((post) => (
                         <Feeds key={post?.id} userImg={post?.author?.profileImgUrl} 
@@ -51,11 +29,5 @@ export default function Dashboard () {
                         />
                      ))}
                 </div>
-            </div>
-            <div className="col-span-3 mt-10 sticky top-0 h-screen">
-                <WhatsHappening />
-            </div>
-        </div>
-     </div>    
     )
 }
