a013d5a64743dc0f8b351e1e716f5c6ee2b69431
Anirban Kapil <anirbankapil@gmail.com>
2026-06-04 20:59:27 +0530

    fix(img-upload): correct file type assertion and improve error handling in image upload

--- app/api/img-upload/route.ts
+++ app/api/img-upload/route.ts
@@ -25,13 +25,33 @@ export async function POST (request : NextRequest) {
 
        try {
          const formData = request.formData();
-         const file = (await formData).get("file") as File || null;
+         const file = (await formData).get("file") as File | null;
          if(!file){
             return NextResponse.json(
                 {error : "Image file not found"},
                 {status : 401}
             )
          } 
+
+         const bytes = await file.arrayBuffer();
+         const buffer = Buffer.from(bytes);
+
+         const result =await new Promise<CloudinaryUploadResult>((resolve,reject) => {
+            const uploadStream = cloudinary.uploader.upload_stream(
+                {folder : "social-media-uploads"},
+                (error,result)=>{
+                    if(error) reject(error);
+                    else resolve(result as CloudinaryUploadResult) 
+                }
+            )
+            uploadStream.end(buffer)
+         })
+          return NextResponse.json(
+                   {
+                    publicId : result.public_id,
+                     imageUrl: result.secure_url
+                   },
+                     {status : 200})
        } catch (error) {
           console.log("error msg -",error)
           return NextResponse.json(
