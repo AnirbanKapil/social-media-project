abf38be909fad3eba97d4aae5de5b1df7baaeb79
Anirban Kapil <anirbankapil@gmail.com>
2026-06-30 11:56:38 +0530

    feat: integrate message module into GraphQL schema and resolvers

---
+++ graphql/modules/message/index.ts
@@ -0,0 +1,9 @@
+import { messageResolvers } from "./resolvers";
+import { messageTypeDefs } from "./typeDefs";
+
+
+
+export const messageModule = {
+    typeDefs: messageTypeDefs,
+    resolvers: messageResolvers
+}
--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,14 +1,17 @@
 
 import { userModule } from "./modules/user";
 import { postModule } from "./modules/post";
+import { messageModule } from "./modules/message";
 
 export const typeDefs = [
   userModule.typeDefs,
-  postModule.typeDefs
+  postModule.typeDefs,
+  messageModule.typeDefs
 ];
 
 export const resolvers = [
   userModule.resolvers,
-  postModule.resolvers
+  postModule.resolvers,
+  messageModule.resolvers,
 ];
 
