14ca6cd7617a5e85e32da408041504772b3b41e1
Anirban Kapil <anirbankapil@gmail.com>
2026-07-04 21:41:43 +0530

    feat: enhance MessagesPage to manage selected conversation state

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,11 +1,19 @@
 import ChatWindow from "../src/components/message/ChatWindow";
 import ConversationList from "../src/components/message/ConversationList";
+import { useState } from "react";
 
 export default function MessagesPage() {
+
+  const [selectedConversationId, setSelectedConversationId] = useState<string | null>(null);
+  
+  
   return (
     <div className="flex h-screen">
-         <ConversationList />
-         <ChatWindow />
+        <ConversationList
+        selectedConversationId={selectedConversationId}
+        setSelectedConversationId={setSelectedConversationId}
+        />
+         <ChatWindow conversationId={selectedConversationId}/>
     </div>
   );
 }
