4ac136fbec7e64769e031038819edaef13745dfa
Anirban Kapil <anirbankapil@gmail.com>
2026-07-05 17:26:44 +0530

    feat: update ChatWindow component to accept conversationId prop and display conversation details

--- app/src/components/message/ChatWindow.tsx
+++ app/src/components/message/ChatWindow.tsx
@@ -1,10 +1,19 @@
 
+type Props = {
+  conversationId: string | null;
+};
 
-
-export default function ChatWindow() {
-  return (
+export default function ChatWindow({conversationId} : Props) {
+   if (!conversationId) {
+    return (
+      <div className="flex-1 flex items-center justify-center">
+        Select a conversation
+      </div>
+    );
+  }
+   return (
     <div className="flex-1">
-      Chat Window
+      Conversation: {conversationId}
     </div>
   );
 }
