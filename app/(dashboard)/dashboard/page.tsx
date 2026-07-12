4ad14db2c4cb3fd1ff457594dfd05ae5b96be5a0
Anirban Kapil <anirbankapil@gmail.com>
2026-07-12 12:10:18 +0530

    feat: add likesCount and isLiked props to Feeds component for enhanced post interaction

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -37,6 +37,8 @@ export default function FeedsPage () {
                         user={post?.author?.username || "Unknown User"}
                         imgSrc={post?.imgURL}
                         created={post?.createdAt || "unknown"}
+                        likesCount={post?.likesCount}
+                        isLiked={post?.isLiked}
                         />
                      ))}
                 </div>
--- app/src/components/feeds.tsx
+++ app/src/components/feeds.tsx
@@ -6,19 +6,14 @@ import { BsUpload } from "react-icons/bs";
 import { FaRegBookmark } from "react-icons/fa6";
 import Link from "next/link";
 import { CldImage } from 'next-cloudinary'
-import { useGetAllPostsQuery } from "@/lib/generated";
 
 
-export function Feeds ({content, userImg, user, imgSrc, created} : 
+
+export function Feeds ({content, userImg, user, imgSrc, created, likesCount, isLiked} : 
     {content : string, userImg? : string | null,
      user : string, imgSrc : string | null |  undefined,
-      created : string,}) {
+      created : string, likesCount : number | undefined, isLiked : boolean | undefined}) {
     
-    const {data, isLoading, error} = useGetAllPostsQuery({});
-    
-    const post = data?.getAllPosts
-
-    const handleLike = () => {}
 
     return(
         <div className="grid grid-cols-12 border-b border-gray-600">
@@ -31,7 +26,7 @@ export function Feeds ({content, userImg, user, imgSrc, created} :
                    <Link href={`/dashboard/${user}`}
                     className="inline-block font-semibold cursor-pointer hover:scale-110 transition-transform duration-300">
                        {user}
-                    </Link>
+                   </Link>
                     <p className="text-slate-400 text-xs mt-2">{new Date(Number(created)).toLocaleString()}</p>
                     </div>
                     <p className="mt-2">{content}</p>
@@ -46,9 +41,9 @@ export function Feeds ({content, userImg, user, imgSrc, created} :
                 <div className="flex justify-between items-center mt-2 w-1/2">
                     <div className="cursor-pointer hover:scale-120 transition-transform duration-300"><LuMessageCircle /></div>
                     <div className="cursor-pointer hover:scale-120 transition-transform duration-300"><AiOutlineRetweet /></div>
-                    <div className="cursor-pointer hover:scale-120 transition-transform duration-300"
-                      onClick={handleLike} 
-                    ><FaRegHeart /></div>
+                    
+                    <div className={`cursor-pointer hover:scale-120 transition-transform duration-300`}><FaRegHeart className={isLiked ? "text-red-700" : "text-blue-300"}/></div>
+                    <p>{likesCount}  Likes</p> 
                     <div className="cursor-pointer hover:scale-120 transition-transform duration-300"><BsUpload /></div>
                     <div className="cursor-pointer hover:scale-120 transition-transform duration-300"><FaRegBookmark /></div>
                 </div>    
